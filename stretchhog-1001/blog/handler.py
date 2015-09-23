90c00ecff89e0722cdacf1aaf6ce5c03a608fb47
Tim van Cann <tvancann@thunderhead.com>
2015-09-23 21:46:25 +0200

    Removed obsolete dependencies

--- stretchhog-1001/blog/handler.py
+++ stretchhog-1001/blog/handler.py
@@ -1,26 +1,26 @@
 from google.appengine.ext.ndb.key import Key
 
-from blog.forms import CategoryForm, TagForm, EntryForm, CommentForm
+from blog.forms import CategoryForm, TagForm, EntryForm
 from blog.models import Entry, Category, Comment
-from flask import make_response, render_template, request, redirect, Response
+from flask import make_response, render_template, request, Response
 from blog import service
 from blog.view import TagView, CategoryView, EntryView, CommentView
 from flask.ext.restful import Resource
 from main import api
-from flask import Markup, jsonify, json
+from flask import Markup, json
 import markdown
 
 
 categories = {1: "Music", 2: "Artificial Intelligence", 3: "Fitness & Health"}
 
 
-def __get_response_for(key, view):
+def get_response_for(key, view):
 	entity = service.get_by_urlsafe_key(key)
 	view = view(entity).__dict__
 	return Response(json.dumps(view), 201, mimetype='application/json')
 
 
-def __put_response_for(key, view, form, update_function):
+def put_response_for(key, view, form, update_function):
 	if form.validate():
 		key = update_function(key, form)
 		view = view(key.get()).__dict__
@@ -29,12 +29,12 @@ def __put_response_for(key, view, form, update_function):
 		return Response(status=400, mimetype='application/json')
 
 
-def __delete_response_for(key, delete_function):
+def delete_response_for(key, delete_function):
 	delete_function(key)
 	return Response(status=201)
 
 
-def __post_response_for(form, view, create_function):
+def post_response_for(form, view, create_function):
 	if form.validate():
 		key = create_function(form)
 		view = view(key.get()).__dict__
@@ -43,70 +43,36 @@ def __post_response_for(form, view, create_function):
 		return Response(status=400, mimetype='application/json')
 
 
-def __template_response_for(template):
+def template_response_for(template):
 	return make_response(render_template(template))
 
 
-def __get_form(form, req):
+def get_form(form, req):
 	return form(data=req.get_json())
 
 
-class EntryCreate(Resource):
+class EntryRUD(Resource):
 	def get(self, key):
-		form = EntryForm(key)
-		return make_response(render_template('blog/entry/create.html', form=form))
-
-	def post(self, key):
-		service.create_entry(EntryForm(key, data=request.get_json()))
-		return redirect(api.url_for(EntryCreate, key=key))
-
-
-class EntryUpdate(Resource):
-	def get(self, key, cat_key):
-		form = EntryForm(cat_key)
-		form = service.update_entry_form(form, key)
-		return make_response(render_template('blog/entry/create.html', form=form))
-
-	def post(self, key, cat_key):
-		service.update_entry(key, EntryForm(cat_key, data=request.get_json()))
-		return redirect(api.url_for(EntryCreate, key=cat_key))
-
-
-class EntryDelete(Resource):
-	def get(self, key):
-		service.delete_entry(key)
-		return redirect(api.url_for(EntryCreate, key=key))
-
-
-class EntryDetail(Resource):
-	@staticmethod
-	def get_entry(key):
 		entry = service.get_by_urlsafe_key(key)
 		entry.post = Markup(markdown.markdown(entry.post))
-		return entry
-
-	def get(self, key):
-		entry = self.get_entry(key)
-		comment_form = CommentForm()
-		return make_response(render_template('blog/entry/entry.html', entry=entry, form=comment_form))
+		view = EntryView(entry).__dict__
+		return Response(view, 200, mimetype='application/json')
 
 
-	def post(self, key):
-		comment_form = CommentForm()
-		form = CommentForm(data=request.get_json())
-		entry = self.get_entry(key)
-		if comment_form.validate():
-			if request.data['parent_comment']:
-				key = request.data['parent_comment']
-			service.create_comment(key, form)
-		return make_response(render_template('blog/entry/entry.html', entry=entry, form=comment_form))
+	def put(self, key):
+		return put_response_for(key, EntryView, get_form(EntryForm, request), service.update_entry)
+
+	def delete(self, key):
+		return delete_response_for(key, service.delete_entry)
 
 
-class EntryList(Resource):
+class EntryCL(Resource):
 	def get(self):
 		entries = service.get_all_entries()
 		view = [EntryView(entry) for entry in entries]
-		return jsonify(view)
+
+	def post(self):
+		post_response_for(get_form(EntryForm, request), EntryView, service.create_entry)
 
 
 class EntryListCategory(Resource):
@@ -126,19 +92,19 @@ class EntrySearch(Resource):
 
 class CategoryTemplate(Resource):
 	def get(self):
-		return __template_response_for('blog/category/list.html')
+		return template_response_for('blog/category/list.html')
 
 
 class CategoryRUD(Resource):
 	def get(self, key):
-		return __get_response_for(key, CategoryView)
+		return get_response_for(key, CategoryView)
 
 	def delete(self, key):
-		return __delete_response_for(key, service.delete_category)
+		return delete_response_for(key, service.delete_category)
 
 
 	def put(self, key):
-		return __put_response_for(key, CategoryView, __get_form(CategoryForm, request), service.update_category)
+		return put_response_for(key, CategoryView, get_form(CategoryForm, request), service.update_category)
 
 
 class CategoryCL(Resource):
@@ -148,23 +114,23 @@ class CategoryCL(Resource):
 		return Response(json.dumps(view), 200, mimetype='application/json')
 
 	def post(self):
-		return __post_response_for(__get_form(CategoryForm, request), CategoryView, service.create_category)
+		return post_response_for(get_form(CategoryForm, request), CategoryView, service.create_category)
 
 
 class TagTemplate(Resource):
 	def get(self):
-		return __template_response_for('blog/tag/list.html')
+		return template_response_for('blog/tag/list.html')
 
 
 class TagRUD(Resource):
 	def get(self, key):
-		return __get_response_for(key, TagView)
+		return get_response_for(key, TagView)
 
 	def put(self, key):
-		return __put_response_for(key, TagView, __get_form(TagForm, request), service.update_tag)
+		return put_response_for(key, TagView, get_form(TagForm, request), service.update_tag)
 
 	def delete(self, key):
-		return __delete_response_for(key, service.delete_tag)
+		return delete_response_for(key, service.delete_tag)
 
 
 class TagCL(Resource):
@@ -175,7 +141,7 @@ class TagCL(Resource):
 		return Response(json.dumps(sorted_view), 200, mimetype='application/json')
 
 	def post(self):
-		return __post_response_for(__get_form(TagForm, request), TagView, service.create_tag)
+		return post_response_for(get_form(TagForm, request), TagView, service.create_tag)
 
 
 class CommentList(Resource):
@@ -185,11 +151,6 @@ class CommentList(Resource):
 		return view
 
 
-api.add_resource(EntryCreate, '/blog/admin/entry/create/<string:key>', endpoint='create_entry')
-api.add_resource(EntryUpdate, '/blog/admin/entry/update/<string:key>/<string:cat_key>', endpoint='update_entry')
-api.add_resource(EntryDelete, '/blog/admin/entry/delete/<string:key>', endpoint='delete_entry')
-api.add_resource(EntryDetail, '/blog/entry/<string:key>', endpoint='get_entry')
-api.add_resource(EntryList, '/blog/entry/list', endpoint='list_entry')
 api.add_resource(EntryListCategory, '/blog/entry/list/<int:cat>', endpoint='category_entry')
 
 api.add_resource(CommentList, '/blog/comment/list/<string:key>', endpoint='list_comment')
